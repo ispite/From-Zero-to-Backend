35ef192568d3681ecee424d8e2930fde0bf93bc4
Denis Solovyov <infinityx93@gmail.com>
2025-03-29 18:31:44 +0300

    create project

---
+++ src/main/kotlin/com/example/fztb/FZtBApplication.kt
@@ -0,0 +1,11 @@
+package com.example.fztb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FZtBApplication
+
+fun main(args: Array<String>) {
+    runApplication<FZtBApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/fztb/FZtBApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.fztb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FZtBApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
